ed38bb23a5cd62d490d3b425f3a5ddc49cd2c41a
Luke Daniel <lddaniel08@gmail.com>
2025-01-16 03:10:33 +0800

    style(create): make title and subtitle spacing tighter

--- src/components/create/CreatePage.tsx
+++ src/components/create/CreatePage.tsx
@@ -120,7 +120,7 @@ export default function CreatePage() {
             </motion.div>
 
             <motion.h1 
-              className="text-2xl font-semibold mb-1"
+              className="text-2xl font-semibold mb-0.5"
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.2, duration: 0.6 }}
